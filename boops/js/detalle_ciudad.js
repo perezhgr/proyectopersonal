69530b54a3900c33f5d98ed49d39a5fcb7d55154
perezhgr <perez.hgr@gmail.com>
2014-09-11 19:12:46 -0300

    Avanzando....

--- boops/js/detalle_ciudad.js
+++
@@ -1,9 +0,0 @@
-function DetalleCiudad(ciudad){
-	$.ajax({
-		type: "GET",
-		url: "detalle_ciudad.php?id_ciudad="+ciudad,
-		success: function(data)
-			{
-				document.getElementById('contenido').innerHTML=data;
-			}});
-}
--- boops/js/q.js
+++
@@ -1,7 +0,0 @@
-function priceSorter(a, b) {
-        a = +a.substring(1); // remove $
-        b = +b.substring(1);
-        if (a > b) return 1;
-        if (a < b) return -1;
-        return 0;
-    }
